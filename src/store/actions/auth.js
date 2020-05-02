09dd52d0bc1877063ebf1eb5be7f2e3b196d11b6
codershona <falgunimst95@gmail.com>
2020-05-02 14:32:13 +0200

    Authnetication added into my firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDu56rOe0KWP0SWnTZgm3bJwecKWo0I9fs';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCmA45HyDNoNYWboXv9L8QVGyuihHza9Z4';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDu56rOe0KWP0SWnTZgm3bJwecKWo0I9fs';
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCmA45HyDNoNYWboXv9L8QVGyuihHza9Z4';
         }
         axios.post(url, authData)
             .then(response => {
